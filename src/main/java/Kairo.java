3886f5d829ce1b0a2c55ac6d8234258172237346
Tian Le <tianle@Tians-MacBook-Pro.local>
2026-09-01 04:47:34 +0800

    Add task status updates

--- src/main/java/Kairo.java
+++ src/main/java/Kairo.java
@@ -9,13 +9,15 @@ public class Kairo {
             "____________________________________________________________";
 
     /**
-     * Starts the chatbot and allows the user to add and list tasks.
+     * Starts the chatbot and allows the user to add, list, mark, and unmark
+     * tasks.
      *
      * @param args command-line arguments
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
 
         System.out.println(HORIZONTAL_LINE);
@@ -34,12 +36,33 @@ public class Kairo {
                 System.out.println(HORIZONTAL_LINE);
 
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String statusIcon = isDone[i] ? "X" : " ";
+                    System.out.println(
+                            (i + 1) + ".[" + statusIcon + "] " + tasks[i]);
                 }
 
                 System.out.println(HORIZONTAL_LINE);
+            } else if (input.startsWith("mark ")) {
+                int taskIndex =
+                        Integer.parseInt(input.substring("mark ".length())) - 1;
+                isDone[taskIndex] = true;
+
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + tasks[taskIndex]);
+                System.out.println(HORIZONTAL_LINE);
+            } else if (input.startsWith("unmark ")) {
+                int taskIndex =
+                        Integer.parseInt(input.substring("unmark ".length())) - 1;
+                isDone[taskIndex] = false;
+
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[ ] " + tasks[taskIndex]);
+                System.out.println(HORIZONTAL_LINE);
             } else {
                 tasks[taskCount] = input;
+                isDone[taskCount] = false;
                 taskCount++;
 
                 System.out.println(HORIZONTAL_LINE);
